47301a2387009dbbafd8649e59acdd82250dac1d
maruduke <abab1117@naver.com>
2025-03-19 20:48:33 +0900

    QR 코드 생성 png 이미지 Byte배열로 클라이언트 전송

--- src/main/kotlin/com/example/kotlin/QR/QRApi.kt
+++ src/main/kotlin/com/example/kotlin/QR/QRApi.kt
@@ -1,16 +1,26 @@
 package com.example.kotlin.QR
 
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.awt.image.BufferedImage
 
 @RestController
 class QRApi(val qrService: QRService) {
 
     @PostMapping("qr")
-    fun QRmake(@RequestBody qrDto: QRDto) : Unit {
-        qrService.QRMake(qrDto.url);
+    fun QRmake(@RequestBody qrDto: QRDto) : ResponseEntity<ByteArray> {
+        val image : ByteArray = qrService.QRMake(qrDto.url);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .contentType(MediaType.IMAGE_PNG)
+            .body(image);
     }
 
 }
--- src/main/kotlin/com/example/kotlin/QR/QRService.kt
+++ src/main/kotlin/com/example/kotlin/QR/QRService.kt
@@ -8,15 +8,22 @@ import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
 import org.springframework.stereotype.Service
 import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
 
 @Service
 class QRService {
-    fun QRMake(url: String) : BufferedImage {
+    fun QRMake(url: String) : ByteArray {
         val qrCodeWriter: QRCodeWriter = QRCodeWriter();
         var hints: HashMap<EncodeHintType, Any> = HashMap();
         hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
         var QR : BitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, 512, 512, hints);
 
-        return MatrixToImageWriter.toBufferedImage(QR, MatrixToImageConfig());
+        val image: BufferedImage = MatrixToImageWriter.toBufferedImage(QR, MatrixToImageConfig());
+
+        val outputStream: ByteArrayOutputStream = ByteArrayOutputStream();
+        ImageIO.write(image, "PNG", outputStream);
+
+        return outputStream.toByteArray();
     }
 }
